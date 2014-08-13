a3c29982be12d915c09de4d0914f583c262f2113
matt454357 <matt454357@gmail.com>
2014-08-12 20:51:09 -0600

    Replaced account_check_writing with the jam version (Jigar Amin)

--- addons/account_check_writing/__init__.py
+++ addons/account_check_writing/__init__.py
@@ -18,9 +18,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+import res_company
 import account
 import account_voucher
 import wizard
 import report
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- addons/account_check_writing/account.py
+++ addons/account_check_writing/account.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2004-Today OpenERP S.A. (<http://www.openerp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,30 +19,77 @@
 #
 ##############################################################################
 
-from openerp.osv import osv,fields
+from openerp import SUPERUSER_ID
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
 
 class account_journal(osv.osv):
     _inherit = "account.journal"
 
     _columns = {
-        'allow_check_writing': fields.boolean('Allow Check writing', help='Check this if the journal is to be used for writing checks.'),
-        'use_preprint_check': fields.boolean('Use Preprinted Check', help='Check if you use a preformated sheet for check'),
+        'allow_check_writing': fields.boolean('Allow Check Writing',
+                help='Check this to be able to write checks using this journal.'),
+        'use_preprint_check': fields.boolean('Use Preprinted Checks',
+                help="Check this if your checks already have a number preprinted on them. Otherwise check numbers will be printed on paper."),
+        'check_sequence_id': fields.many2one('ir.sequence', 'Check Sequence',
+                help="This field contains the information related to the numbering of the Check of this journal."),
         }
 
+    def configure_check_journal(self, cr, uid, context=None):
+        '''
+        This function set up bank journal to be used on check payment by
+        setting allow check writing on bank type journals this function will
+        be triggered while installing module.
+        '''
+        journal_ids = self.search(cr, uid, [('type', '=', 'bank')], context=context)
+        if journal_ids:
+            self.write(cr, uid, journal_ids, {'allow_check_writing': True}, context=context)
+        return True
 
-class res_company(osv.osv):
-    _inherit = "res.company"
-    _columns = {
-        'check_layout': fields.selection([
-            ('top', 'Check on Top'),
-            ('middle', 'Check in middle'),
-            ('bottom', 'Check on bottom'),
-            ],"Check Layout",
-            help="Check on top is compatible with Quicken, QuickBooks and Microsoft Money. Check in middle is compatible with Peachtree, ACCPAC and DacEasy. Check on bottom is compatible with Peachtree, ACCPAC and DacEasy only"  ),
+    def onchange_journal_type(self, cr, uid, ids, type, context=None):
+        """
+        on change journal type enable the allow_check_writing flag to allow
+        configuration of check journal.
+        """
+        return {'value': {'allow_check_writing': type == "bank"}}
+
+    def create_check_sequence(self, cr, uid, vals, context=None):
+        """
+        Create new no_gap entry sequence for check Journal using given vals.
+        """
+        val = {
+            'name': vals['name'] + _(" : Check Number Sequence"),
+            'implementation': 'no_gap',
+            'padding': 4,
+            'number_increment': 1
         }
-        
-    _defaults = {
-        'check_layout' : lambda *a: 'top',
-    }
-    
+        if 'company_id' in vals:
+            val['company_id'] = vals['company_id']
+        return self.pool.get('ir.sequence').create(cr, uid, val, context)
+
+    def create(self, cr, uid, vals, context=None):
+        if not 'check_sequence_id' in vals or not vals['check_sequence_id']  and vals.get('allow_check_writing'):
+            # if we have the right to create a journal, we should be able to
+            # create it's check number sequence.
+            vals.update({'check_sequence_id': self.create_check_sequence(cr, SUPERUSER_ID, vals, context)})
+        return super(account_journal, self).create(cr, uid, vals, context)
+
+
+class tmodel_multi_charts_accounts(osv.TransientModel):
+
+    _inherit = 'wizard.multi.charts.accounts'
+
+    def _prepare_bank_journal(self, cr, uid, line, current_num, default_account_id, company_id, context=None):
+        '''
+        This function prepares the value to use for the creation of a bank and
+        addes the allow_check_writing flag value for Bank Type journal
+        :rtype: dict
+        '''
+        vals = super(tmodel_multi_charts_accounts, self)._prepare_bank_journal(cr, uid, line, current_num, default_account_id, company_id, context=context)
+        if line['account_type'] == "bank":
+            vals.update({'allow_check_writing': True})
+        return vals
+
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
