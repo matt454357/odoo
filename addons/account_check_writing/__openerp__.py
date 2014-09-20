1433d73e25efe31f8b6b6ab09df750b8ef4dc22c
matt454357 <matt454357@gmail.com>
2014-09-20 07:31:21 -0600

    replaced account_check_writing with jam version

--- addons/account_check_writing/__openerp__.py
+++ addons/account_check_writing/__openerp__.py
@@ -21,25 +21,42 @@
 {
     'name': 'Check Writing',
     'version': '1.1',
-    'author': 'OpenERP SA, NovaPoint Group',
+    'author': 'OpenERP SA, Ursa Information Systems',
     'category': 'Generic Modules/Accounting',
     'description': """
-Module for the Check Writing and Check Printing.
-================================================
+With this module, you will be able to write checks to your vendors in OpenERP.
+==============================================================================
+
+This module supports the following:
+-----------------------------------
+    * Multiple currencies: it is possible to write checks in different currencies.
+    * Multiple journals: each check can use a different payment method.
+    * Multiple formats: checks can be printed on top, in the middle or at the bottom to fit Quickbooks/Peachtree/... templates.
+    * Batch check number assignation.
+    * Batch printing of checks.
+    * Complete check number sequence management: number auto-suggestion, sequence recalibration, optional overwrite if already specified, duplicates detection.
+    * Optional printing of check number: if the paper checks of your bank already include numbers, you can use these numbers instead of OpenERP’s check numbers.
+    * Optional spill-over on multiple stubs: when the check includes more than 10 invoice lines, they will be printed  on several pages.
+    * Optional printing of credits.
+    * Integration with payments workflow: checks are an integrated part of the payment workflow in OpenERP.
+    * Comprehensive and accurate information displayed on stub: supplier's invoice number, void status, memo, suppresses invoices that are not paid with this check.
     """,
-    'website': 'https://www.odoo.com/page/accounting',
-    'depends' : ['account_voucher'],
+    'website': 'http://www.openerp.com',
+    'depends': ['account_voucher'],
     'data': [
         'wizard/account_check_batch_printing_view.xml',
         'account_view.xml',
         'account_voucher_view.xml',
+        'res_company_view.xml',
         'account_check_writing_data.xml',
-        'data/report_paperformat.xml',
-        'views/report_check.xml',
-        'account_check_writing_report.xml',
     ],
-    'demo': ['account_demo.xml'],
-    'test': [],
+    'demo': [
+        'account_check_writing_demo.xml',
+        'account_check_writing_demo.yml',
+        ],
+    'test': [
+        'test/account_check_writing_report.yml'
+    ],
     'installable': True,
     'active': False,
 }
